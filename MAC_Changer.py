7beef8c14a2d528a8cee447c3b0eb6801d68636f
nivios <nivios7@gmail.com>
2019-06-07 12:23:21 -0700

    Update MAC_Changer.py

--- MAC_Changer.py
+++ MAC_Changer.py
@@ -2,6 +2,11 @@
 
 import subprocess
 
-subprocess.call("ifconfig eth0 down", shell=True)
-subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:55", shell=True)
-subprocess.call("ifconfig eth0 up", shell=True)
+interface = "eth0"
+mac = "00:11:22:33:44:55"
+
+print("[+] Changing MAC address for " + interface + " to " + mac + ".")
+
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + mac, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
